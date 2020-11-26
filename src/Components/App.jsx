882fd3a4921fc77ad19dfa7451a59fc56a9d1be7
Mehdi Bentouati <mehdi.bentouati76@gmail.com>
2020-11-26 14:53:29 +0100

    add route webcamdetection

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import '../styles/App.css'
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div className='app'>
-      <Link to={'/airbnb'}>
-        <div className='app__airbnb'>
-          AirBnb
-        </div>
+    <div className="app">
+      <Link to={"/airbnb"}>
+        <div className="app__airbnb">AirBnb</div>
       </Link>
-      <Link to={'/reconnaissance'}>
-        <div className='app__reconnaissance'>
-        Reconnaissance d'image
-        </div>
+      <Link to={"/reconnaissance"}>
+        <div className="app__reconnaissance">Reconnaissance d'image</div>
+      </Link>
+      <Link to={"/webcamDetection"}>
+        <div className="app__reconnaissance">Utilisation de la webcam</div>
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
