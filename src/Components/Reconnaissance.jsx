ffd0a0f1b2a3c89fd35f9f55dcc68dc4647bcef2
Mehdi Bentouati <mehdi.bentouati76@gmail.com>
2020-11-26 08:46:35 +0100

    ajout data

--- src/Components/Reconnaissance.jsx
+++ src/Components/Reconnaissance.jsx
@@ -19,18 +19,44 @@ function ModifyDetail() {
   async function handleFile(e) {
     const img = document.getElementById("img_predict");
     img.src = URL.createObjectURL(e.target.files[0]);
-    console.log(e.target.files[0]);
+    img.title = e.target.files[0].name;
   }
 
   async function reconn() {
     const prediction = await model.classify(
       document.getElementById("img_predict")
     );
+    let imgToB64 = convertB64(document.getElementById("img_predict"));
     const predict = {
-      nom: prediction[0].className,
-      probabilite: prediction[0].probability * 100,
+      nom: document.getElementById("img_predict").title,
+      analyse: {
+        taux: prediction[0].probability * 100,
+        type: prediction[0].className,
+      },
+      image: imgToB64,
+      date: getToday(),
+      size: imgToB64.length * (3 / 4) - 2,
     };
     console.log("predictions : ", predict);
+  }
+
+  function convertB64(img) {
+    let canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    let ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0, 100, 100);
+    let dataURL = canvas.toDataURL("image/png");
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+  }
+
+  function getToday() {
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, "0");
+    let mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+    let yyyy = today.getFullYear();
+
+    return yyyy + "-" + mm + "-" + dd;
   }
 
   return (
@@ -39,6 +65,7 @@ function ModifyDetail() {
       <br />
       <img id="img_predict" height="200" width="200" />
       <br />
+      <img height="200" width="200" src="data:image;base64, " />
       <button onClick={reconn}>Reconnaissance</button>
     </>
   );
