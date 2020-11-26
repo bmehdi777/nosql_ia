d5a64af820b3c5a214a2e79a723f7ef1fb47ca7e
Théo Dieppois <contact@theodieppois.fr>
2020-11-26 08:37:28 +0100

    front

---
+++ src/Components/DragAnDropFile/DragAnDropFile.jsx
@@ -0,0 +1,85 @@
+import React, {useEffect, useRef} from 'react'
+import './DragAnDropFile.css'
+
+const DragAnDropFile = ({onChange}) => {
+
+    let dragBox = useRef(null)
+
+
+    const updateThumbnail = (file) => {
+        let thumbnailElem = document.querySelector('.drop-zone__thumb')
+
+        if(document.querySelector('.drop-zone__prompt')) {
+            document.querySelector('.drop-zone__prompt').remove()
+        }
+
+        if(!thumbnailElem) {
+            thumbnailElem = document.createElement('div')
+            thumbnailElem.classList.add('drop-zone__thumb')
+            document.querySelector('.drop-zone').appendChild(thumbnailElem)
+        }
+        thumbnailElem.dataset.label = file.name
+
+        if(file.type.startsWith('image/')) {
+            const reader = new FileReader()
+            reader.readAsDataURL(file)
+            reader.onload = () => {
+                thumbnailElem.style.backgroundImage = `url('${reader.result}')`
+            }
+        } else {
+            thumbnailElem.style.backgroundImage = null
+        }
+    }
+
+    const uploadImage = (e) => {
+        updateThumbnail(e.target.files[0])
+        console.log('changed')
+        onChange(e.target.files[0])
+    }
+
+    useEffect(() => {
+        dragBox.addEventListener('dragover', e => {
+            e.preventDefault()
+            document.querySelector('.drop-zone').classList.add('drop-zone--hover')
+        })
+
+        dragBox.addEventListener('dragleave', e => {
+            document.querySelector('.drop-zone').classList.remove('drop-zone--hover')
+        })
+
+        dragBox.addEventListener('dragend', e => {
+            document.querySelector('.drop-zone').classList.remove('drop-zone--hover')
+        })
+
+        dragBox.addEventListener('drop', e => {
+            e.preventDefault()
+
+            if(e.dataTransfer.files.length) {
+                document.querySelector('.drop-zone__input').files = e.dataTransfer.files
+                updateThumbnail(e.dataTransfer.files[0])
+                //car onChange de l'imput pas activé
+                onChange(e.dataTransfer.files[0])
+            }
+
+            document.querySelector('.drop-zone').classList.remove('drop-zone--hover')
+        })
+    },[])
+
+    return (
+        <div 
+            className='drop-zone'
+            ref={el => (dragBox = el)}
+            onClick={() => document.querySelector('.drop-zone__input').click()}
+        >
+            <span className='drop-zone__prompt'>Drop une image ici ou clique pour upload</span> 
+    
+            <input 
+                type="file" 
+                className='drop-zone__input'
+                onChange={(e) => uploadImage(e)}
+            />
+        </div>
+    )
+}
+
+export default DragAnDropFile
--- src/Components/Reconnaissance.jsx
+++ src/Components/Reconnaissance.jsx
@@ -1,46 +1,68 @@
 import React, { useEffect, useState } from "react";
-import { useParams, useHistory } from "react-router-dom";
 import axios from "axios";
 import parse from "html-react-parser";
 import * as tf from "@tensorflow/tfjs";
 import * as mobilenet from "@tensorflow-models/mobilenet";
+import DragAnDropFile from "./DragAnDropFile/DragAnDropFile";
+
+import './Reconnaissance.css'
 
 const uri = "http://localhost:8080";
 
 function ModifyDetail() {
-  let model;
+  const [model, setModel] = useState()
+  const [prediction, setPrediction] = useState('')
+
+
   useEffect(() => {
     async function loadModel() {
-      model = await mobilenet.load();
+      setModel(await mobilenet.load());
     }
     loadModel();
   }, []);
 
-  async function handleFile(e) {
+
+  const handleFile = async (fileUrl) => {
     const img = document.getElementById("img_predict");
-    img.src = URL.createObjectURL(e.target.files[0]);
-    console.log(e.target.files[0]);
+    img.src = URL.createObjectURL(fileUrl);
   }
 
   async function reconn() {
-    const prediction = await model.classify(
+    const predictions = await model.classify(
       document.getElementById("img_predict")
     );
+
     const predict = {
-      nom: prediction[0].className,
-      probabilite: prediction[0].probability * 100,
+      nom: predictions[0].className,
+      probabilite: predictions[0].probability * 100,
     };
-    console.log("predictions : ", predict);
+
+    axios
+      .post(uri + "/image", {
+        predict: predict,
+      }).then(() => {
+        //resfresh historique
+      }).catch((err) => console.log(err))
+
+    setPrediction(predict.nom)
   }
 
   return (
-    <>
-      <input type="file" onChange={handleFile} />
-      <br />
-      <img id="img_predict" height="200" width="200" />
-      <br />
-      <button onClick={reconn}>Reconnaissance</button>
-    </>
+    <div className='reconnaissance'>
+      <p
+        className='reconnaissance__title'
+      >
+        {prediction ? prediction : ''}
+      </p>
+      <DragAnDropFile onChange={handleFile}/>
+      <img id="img_predict" style={{display: 'none'}} />
+      <button 
+        className='reconnaissance__btn'
+        onClick={reconn}
+        >
+        Reconnaissance
+      </button>
+    </div>
   );
 }
 
